cb0d5ef5b03cedb7409629965ab8f4427d4143cf
Winston Pichardo <winston1420@hotmail.com>
2024-04-15 04:51:58 -0400

    Removed services view (unnecessary)

--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,7 @@ const routes = [
         path: '/',
         name: 'HomeView',
         component: Home
-    },
-    {
-        path: '/servicios',
-        name: 'ServiciosView',
-        component: Home
-    },
+    },   
     {
         path: '/productos',
         name: 'ProductosView',
